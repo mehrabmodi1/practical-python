32b00b962ae420b1605a6769b93d4df3e56922ee
mehrab modi <mehrabmodi@gmail.com>
2020-06-09 15:42:15 -0400

    displays monthly numbers and fixed overpayment on last month.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,6 +3,7 @@
 # Exercise 1.7
 # mortgage.py
 
+
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
@@ -12,16 +13,24 @@ ex_pay_start_m = 60;
 ex_pay_end_m = 108;
 ex_pay_am = 1000;
 
+
 while principal > 0:
    
     if month_n >= ex_pay_start_m and month_n <= ex_pay_end_m:
         payment_i = payment + ex_pay_am;
     else:
         payment_i = payment;
+    new_princ = principal * (1+rate/12);
+    if new_princ < payment_i:
+        payment_i = new_princ;
+            
         
-    principal = principal * (1+rate/12) - payment_i;
-    total_paid = total_paid + payment
+    principal = new_princ - payment_i;
+    total_paid = total_paid + payment_i
+    print([principal, total_paid])
     month_n = month_n + 1;
+    
+    
 
 print('Total paid', total_paid)
 print('final year n ', month_n/12)
