0de2d40f324dd740fe1eda4c6896d7fe383cf4f8
mehrab modi <mehrabmodi@gmail.com>
2020-06-12 15:35:49 -0400

    finished work on reading in portfolio data as a list of tuples.

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,19 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], float(row[1]), float(row[2]))  #this creates the tuple 'holding' that has current row's data
+            portfolio.append(holding)       #this appends the tuple 'holding' to the list 'portfolio'
+    
+    print(portfolio)
+    return portfolio
+
+            
